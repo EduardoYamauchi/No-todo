d6c08a778932cef9811c3803cd090b478001e392
Eduardo Yamauchi <yamauchieduardo@gmail.com>
2018-06-05 23:21:01 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# notodo
+
+A Dart and Flutter base study project, this project has primary objective build a inverse to-do list, thus a list what no-to-do
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/ui/notodo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotoDoScreen extends StatefulWidget {
+  @override
+  _NotoDoScreenState createState() => _NotoDoScreenState();
+}
+
+class _NotoDoScreenState extends State<NotoDoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
