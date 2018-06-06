464f0b852c2cc6dfd5404f42ac3767ee7d9d5fa9
Eduardo Yamauchi <yamauchieduardo@gmail.com>
2018-06-06 18:07:21 -0400

    Added an UI visual cards from db-items

--- lib/ui/notodo_screen.dart
+++ lib/ui/notodo_screen.dart
@@ -8,26 +8,70 @@ class NotoDoScreen extends StatefulWidget {
 }
 
 class _NotoDoScreenState extends State<NotoDoScreen> {
-  final TextEditingController _textEditingController = new TextEditingController();
+  final TextEditingController _textEditingController =
+      new TextEditingController();
   var db = new DatabaseHelper();
+  final List<NoDoItem> _itemList = <NoDoItem>[];
+
+  @override
+  //init state, created on vue
+  void initState() {
+    super.initState();
+
+    _readNoDoList();
+  }
 
   void _handleSubmitted(String text) async {
     _textEditingController.clear();
-    NoDoItem noDoItem = new NoDoItem(text, new DateTime.now().toIso8601String());
+    NoDoItem noDoItem =
+        new NoDoItem(text, new DateTime.now().toIso8601String());
 //    print(noDoItem);
     int saveItemId = await db.saveItem(noDoItem);
     print('Item Saved $saveItemId');
+
+    NoDoItem addedItem = await db.getItem(saveItemId);
+
+    //method for update listview after add a new item
+    setState(() {
+      _itemList.insert(0, addedItem);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(),
-
-
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+              child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                  reverse: false,
+                  itemCount: _itemList.length,
+                  itemBuilder: (context, int index) {
+                    return new Card(
+                      color: Colors.white10,
+                      child: new ListTile(
+                        title: _itemList[index],
+                        onLongPress: () => debugPrint(''),
+                        trailing: new Listener(
+                          key: new Key(_itemList[index].itemName),
+                          child: new Icon(Icons.remove_circle,
+                          color: Colors.redAccent),
+                          onPointerDown: (pointerEvent) => debugPrint('del'),
+                        ),
+                      ),
+                    );
+                  }
+              )
+          ),
+          new Divider(
+            height: 1.0,
+          )
+        ],
+      ),
       floatingActionButton: new FloatingActionButton(
-        tooltip: 'Add Item',
+          tooltip: 'Add Item',
           backgroundColor: Colors.blueAccent,
           child: new ListTile(
             title: new Icon(Icons.add),
@@ -42,14 +86,13 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
         children: <Widget>[
           new Expanded(
               child: new TextField(
-                controller: _textEditingController,
-                autofocus: true,
-                decoration: new InputDecoration(
-                  labelText: 'Item',
-                  hintText: "eg. Don't buy stuff",
-                  icon: new Icon(Icons.note_add)
-                ),
-              ))
+            controller: _textEditingController,
+            autofocus: true,
+            decoration: new InputDecoration(
+                labelText: 'Item',
+                hintText: "eg. Don't buy stuff",
+                icon: new Icon(Icons.note_add)),
+          ))
         ],
       ),
       actions: <Widget>[
@@ -57,14 +100,24 @@ class _NotoDoScreenState extends State<NotoDoScreen> {
             onPressed: () {
               _handleSubmitted(_textEditingController.text);
               _textEditingController.clear();
-    }, child: Text('Save')),
-        new FlatButton(onPressed: () => Navigator.pop(context),
-            child: Text('Cancel'))
+            },
+            child: Text('Save')),
+        new FlatButton(
+            onPressed: () => Navigator.pop(context), child: Text('Cancel'))
       ],
     );
-    showDialog(context: context,
-    builder: (_) {
-      return alert;
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
+  }
+
+  void _readNoDoList() async {
+    List items = await db.getItems();
+    items.forEach((item) {
+      NoDoItem noDoItem = NoDoItem.map(item);
+      print('Db items: ${noDoItem.itemName}');
     });
   }
 }
